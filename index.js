2f54e563530c87b929502fd1cb21d65a83974c24
John Arnone Jr <johnsarnonejr@gmail.com>
2018-12-28 02:42:14 +0000

    Done.

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ function longPlaneteerCalls(words) {
 
 function findTheCheese (foods) {
   for (let i = 0; i < foods.length; i++) {
-     if (foods[i] ==  'gouda' || foods[i] == 'cheddar' || foods[i] == 'camembert') {
+     if (foods[i] ==  "gouda" || foods[i] == "cheddar" || foods[i] == "camembert") {
       return foods[i];
     }
   }
-  return 'no cheese!';
+  return "no cheese!";
 }
